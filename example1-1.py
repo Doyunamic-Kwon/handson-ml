71c05023dd8cb165db4088a0ba9cc650adf4bfd6
Doyun_kwon <kwondoyoon22@gmail.com>
2026-03-10 19:05:00 +0900

    example1-1 update

--- example1-1.py
+++ example1-1.py
@@ -1,7 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import sklearn.linear_model as lm
+from sklearn.linear_model import LinearRegression
 
-oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
-gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values="n/a")
+# 데이터를 다운로드하고 준비합니다
+data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+X = lifesat[["GDP per capita (USD)"]].values
+y = lifesat[["Life satisfaction"]].values
+
+# 데이터를 그래프로 나타냅니다
+lifesat.plot(kind='scatter', grid=True,
+             x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+#  선형 모델를 선택합니다
+model = LinearRegression()
+
+# 모델을 훈련합니다
+model.fit(X, y)
+
+# 키프로스에 대해 예측을 만듭니다
+X_new = [[37_655.2]]  # 2020년 키프로스 1인당 GDP
+print(model.predict(X_new)) # 출력 [[6.30165767]]
